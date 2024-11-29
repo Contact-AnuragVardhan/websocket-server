2e7006f33b564bde309aeed355980a01bd7bcdf7
contact-anurag <contact.anuragvardhan@gmail.com>
2024-11-29 08:12:58 -0500

    Updated Code to handle Room.connect and username for socket

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const io = new Server(server, {
             logger.info(`User connected: ${socket.id}`);
 
             socket.on('user_connected', async ({ username }) => {
-                socket.username = username;
+                handleSocketInitialization(socket, username);
 
                 await pubClient.sAdd(`user:${username}:sockets`, socket.id);
                 logger.info(`New socketId is ${socket.id} for username ${username}`);
@@ -70,8 +70,7 @@ const io = new Server(server, {
                 const userRooms = await pubClient.sMembers(`user:${username}:rooms`);
 
                 for (const room of userRooms) {
-                    socket.join(room);
-                    socket.username = username;
+                    handleSocketInitialization(socket, username, room);
                     logger.info(`User ${username} rejoined room ${room}`);
 
                     /*const latestMessage = await getMessageNotificationForRoom(username, room);
@@ -120,6 +119,8 @@ const io = new Server(server, {
             socket.on('send_message', (data) => {
                 const room = data.room;
                 const username = data.author;
+
+                handleSocketInitialization(socket, username, room);
             
                 pubClient.exists(`room:${room}:users`)
                     .then((roomExists) => {
@@ -160,6 +161,7 @@ const io = new Server(server, {
             });*/
 
             socket.on('get_user_rooms', ({ username }) => {
+                handleSocketInitialization(socket, username);
                 getUserRooms(username, socket);
             });
 
@@ -167,8 +169,8 @@ const io = new Server(server, {
                 getAllUserInRooms(room, socket);
             });
 
-            socket.on('get_room_messages', ({ room }) => {
-                getRoomMessages(room, -1, -1, socket)
+            socket.on('get_room_messages', ({ room, username }) => {
+                getRoomMessages(room, -1, -1, socket, username)
                     .then((messages) => {
                         socket.emit('message_history', { room, messages });
                     })
@@ -177,8 +179,8 @@ const io = new Server(server, {
                     });
             });
             
-            socket.on('get_room_messages_pages', ({ room, page = 1, pageSize = 50 }) => {
-                getRoomMessages(room, page, pageSize, socket)
+            socket.on('get_room_messages_pages', ({ room, page = 1, pageSize = 50, username }) => {
+                getRoomMessages(room, page, pageSize, socket, username)
                     .then((messages) => {
                         socket.emit('message_history_pages', { room, messages, page, pageSize });
                     })
@@ -211,6 +213,7 @@ const io = new Server(server, {
 
             socket.on('update_last_read_message', ({ room, username }) => {
                 logger.info(`Updating Last Read Message for Room ${room} for User ${username}`);
+                handleSocketInitialization(socket, username);
                 
                 pubClient.lLen(`room:${room}:messages`)
                     .then((totalMessages) => {
@@ -252,9 +255,11 @@ const io = new Server(server, {
                         .catch((error) => {
                             logger.error('Error during disconnect:', error);
                         });
+                    logger.info(`User disconnected: ${socket.id}`);
                 }
-                
-                logger.info(`User disconnected: ${socket.id}`);
+                else {
+                    logger.warn(`In disconnect Socket username not set for socket ID ${socket.id}`);
+                }
             });
             
 
@@ -278,11 +283,12 @@ const io = new Server(server, {
 })();
 
 const createRoom = async (room, username, socket, fromEvent) => {
+    handleSocketInitialization(socket, username, room);
     const roomExists = await pubClient.exists(`room:${room}:users`);
     if (!roomExists) {
         logger.info(`Creating Room ${room} in ${fromEvent} event`);
-        socket.join(room);
-        socket.username = username;
+        //socket.join(room);
+        //socket.username = username;
         await pubClient.sAdd(`room:${room}:users`, username);
         await pubClient.sAdd(`user:${username}:rooms`, room);
         await pubClient.sAdd('rooms:set', room);
@@ -302,13 +308,15 @@ const createRoom = async (room, username, socket, fromEvent) => {
 };
 
 const addUserInRoom = async (room, username, socket, fromEvent) => {
+    handleSocketInitialization(socket, username, room);
+    
     const userInRoom = await pubClient.sIsMember(`room:${room}:users`, username);
     if (!userInRoom) {
         logger.info(`User ${username} is not in room ${room}. Adding to room from event ${fromEvent}`);
         await pubClient.sAdd(`room:${room}:users`, username);
         await pubClient.sAdd(`user:${username}:rooms`, room);
-        socket.join(room);
-        socket.username = username;
+        //socket.join(room);
+        //socket.username = username;
         addDefaultMessage(room, username, `${username} joined the ${room}`);
 
         const users = await pubClient.sMembers(`room:${room}:users`);
@@ -333,22 +341,30 @@ const getAllUserInRooms = async (room, socket) => {
 const getUserRooms = async (username, socket) => {
     try {
         const rooms = await pubClient.sMembers(`user:${username}:rooms`);
-        const userRooms = [];
+        const userRooms = await Promise.all(
+            rooms.map(async (room) => {
+                const latestMessage = await getMessageNotificationForRoom(username, room);
+                return { room, latestMessage };
+            })
+        );
+        socket.emit('user_rooms', userRooms);
+        /*const userRooms = [];
         if (rooms.length > 0) {
             for (const room of rooms) {
                 const latestMessage = await getMessageNotificationForRoom(username, room);
                 userRooms.push({ room, latestMessage });
             }
         }
-        socket.emit('user_rooms', userRooms);
+        socket.emit('user_rooms', userRooms);*/
     } catch (error) {
         logger.error('Error getting user rooms:', error);
         socket.emit('error_message', { message: 'Failed to get user rooms.' });
     }
 };
 
-const getRoomMessages = async (room, page = 1, pageSize = 50, socket) => {
+const getRoomMessages = async (room, page = 1, pageSize = 50, socket, username) => {
     try {
+        handleSocketInitialization(socket, username);
         const totalMessages = await pubClient.lLen(`room:${room}:messages`);
 
         const end = totalMessages - (page - 1) * pageSize - 1;
@@ -474,6 +490,18 @@ const getMessageNotificationForRoom = async (username, room) => {
     }
     return null;
 };
+
+const handleSocketInitialization = (socket, username, room) => {
+    if(socket) {
+        if(room) {
+            // Since socket.join(room) is idempotent we can always use socket.join(room) without checking it
+            socket.join(room);
+        }
+        if (username) {
+            socket.username = username;
+        }
+    }
+}
 
 const isValidDate = (dateString) => {
     const date = new Date(dateString);
